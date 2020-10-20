54f2c9a83212f69ac0d3bbae1fe3dac4d8c7bcb5
zeinrezky <zeinrezky@gmail.com>
2020-10-20 15:30:50 +0700

    Initial Commit

---
+++ CustomRoundedZeinFramework/CustomRoundedZeinFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomRoundedZeinFramework.h
+//  CustomRoundedZeinFramework
+//
+//  Created by zein rezky chandra on 20/10/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedZeinFramework.
+FOUNDATION_EXPORT double CustomRoundedZeinFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedZeinFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedZeinFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedZeinFramework/PublicHeader.h>
+
+
